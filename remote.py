080e1f521ee066fa51aa3834b6924879ed511bd6
Erdoğan Yeşil <erdogan.yesil@etiya.com>
2024-02-09 20:23:40 +0300

    first commit

---
+++ remote.py
@@ -0,0 +1,214 @@
+import os
+from flask import Flask, request
+from pynput.mouse import Button, Controller as MouseController
+from pynput.keyboard import Key, Controller as KeyboardController
+import platform
+import subprocess
+import psutil
+
+app = Flask(__name__)
+mouse = MouseController()
+keyboard = KeyboardController()
+
+@app.route('/mouse_move', methods=['POST'])
+def mouse_move():
+    current_location = mouse.position
+    x = int(request.form.get('x')) + current_location[0]
+    y = int(request.form.get('y')) + current_location[1]
+    mouse.position = (int(x), int(y))
+    print( x, y)
+    return "Success", 200
+
+@app.route('/mouse_click', methods=['POST'])
+def mouse_click():
+    button = request.form.get('button')
+    print(button)
+    if button == 'left':
+        mouse.click(Button.left)
+    elif button == 'right':
+        mouse.click(Button.right)
+    elif button == 'middle':
+        mouse.click(Button.middle)
+    elif button == 'double':
+        mouse.click(Button.left, 2)
+    return "Success", 200
+
+@app.route('/keyboard_type', methods=['POST'])
+def keyboard_type():
+    keys = request.form.get('keys')
+    keyboard.type(keys)
+    return "Success", 200
+
+@app.route('/press_key', methods=['POST'])
+def press_key():
+    key = request.form.get('key')
+    print(key)
+    if key == 'play_pause':
+        keyboard.press(Key.media_play_pause)
+        keyboard.release(Key.media_play_pause)
+    elif key == 'volume_up':
+        keyboard.press(Key.media_volume_up)
+        keyboard.release(Key.media_volume_up)
+    elif key == 'volume_down':
+        keyboard.press(Key.media_volume_down)
+        keyboard.release(Key.media_volume_down)
+    elif key == 'next':
+        keyboard.press(Key.media_next)
+        keyboard.release(Key.media_next)
+    elif key == 'previous':
+        keyboard.press(Key.media_previous)
+        keyboard.release(Key.media_previous)
+    elif key == 'mute':
+        keyboard.press(Key.media_volume_mute)
+        keyboard.release(Key.media_volume_mute)
+    elif key == 'fullscreen':
+        keyboard.press(Key.f11)
+        keyboard.release(Key.f11)
+    elif key == 'stop':
+        keyboard.press(Key.stop)
+        keyboard.release(Key.stop)
+    elif key == 'enter':
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+    elif key == 'backspace':
+        keyboard.press(Key.backspace)
+        keyboard.release(Key.backspace)
+    elif key == 'esc':
+        keyboard.press(Key.esc)
+        keyboard.release(Key.esc)
+    elif key == 'tab':
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+    elif key == 'space':
+        keyboard.press(Key.space)
+        keyboard.release(Key.space)
+    elif key == 'up':
+        keyboard.press(Key.up)
+        keyboard.release(Key.up)
+    elif key == 'down':
+        keyboard.press(Key.down)
+        keyboard.release(Key.down)
+    elif key == 'left':
+        keyboard.press(Key.left)
+        keyboard.release(Key.left)
+    elif key == 'right':
+        keyboard.press(Key.right)
+        keyboard.release(Key.right)
+    elif key == 'zoom_in':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('+')
+        keyboard.release('+')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('+')
+        keyboard.release('+')
+        keyboard.release(Key.ctrl_l)
+    elif key == 'zoom_out':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('-')
+        keyboard.release('-')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('-')
+        keyboard.release('-')
+        keyboard.release(Key.ctrl_l)
+    elif key == 'refresh':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('r')
+        keyboard.release('r')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.f5)
+        keyboard.release(Key.f5)
+    elif key == 'new_tab':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.ctrl_l)
+    elif key == 'close_tab':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('w')
+        keyboard.release('w')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('w')
+        keyboard.release('w')
+        keyboard.release(Key.ctrl_l)
+    elif key == 'google':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.ctrl_l)
+        keyboard.type("https://www.google.com")
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+    elif key == 'youtube':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.ctrl_l)
+        keyboard.type("https://www.youtube.com/tv")
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+    elif key == 'netflix':
+        keyboard.press(Key.cmd_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.cmd_l)
+        keyboard.press(Key.ctrl_l)
+        keyboard.press('t')
+        keyboard.release('t')
+        keyboard.release(Key.ctrl_l)
+        keyboard.type("https://www.netflix.com")
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+
+        
+    
+    # Add more keys if needed
+    return "Success", 200
+
+@app.route('/app_list', methods=['POST'])
+def app_list():
+    return get_open_applications(), 200
+def get_open_applications_windows():
+    try:
+        from pywinauto import Application
+        app = Application().connect()
+        return [(proc.process_id(), proc.name()) for proc in app.processes()]
+    except ImportError:
+        print("pywinauto module is not installed. Install it with 'pip install pywinauto'")
+        return []
+
+def get_open_applications_mac():
+    output = subprocess.check_output(['osascript', '-e', 'tell application "System Events" to get name of (processes where background only is false)'])
+    applist =  output.decode('utf-8').split(', ')
+    return [i for i in applist]
+
+
+def get_open_applications():
+    os_name = platform.system()
+    if os_name == 'Windows':
+        return get_open_applications_windows()
+    elif os_name == 'Darwin':
+        return get_open_applications_mac()
+    else:
+        print(f"{os_name} is not supported.")
+        return []
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
---
+++ script.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+  var startX, startY;
+  let isMove = false;
+
+  $("#mouseBox").on("touchstart", function (event) {
+    event.preventDefault();
+    var touch = event.originalEvent.touches[0];
+    startX = touch.pageX;
+    startY = touch.pageY;
+  });
+
+  $("#mouseBox").on("touchmove", function (event) {
+    event.preventDefault();
+    isMove = true;
+    var touch = event.originalEvent.touches[0];
+    var x = touch.pageX - startX;
+    var y = touch.pageY - startY;
+    startX = touch.pageX;
+    startY = touch.pageY;
+    const speed = $("#mouseSpeed").val();
+    console.log(speed);
+    $.post("http://192.168.31.130:5001/mouse_move", { x: x * speed, y: y * speed }, function (data, status) {
+      console.log("Data: " + data + "\nStatus: " + status);
+    });
+  });
+
+  let lastTouchEnd = 0;
+  let count = 0;
+  $("#mouseBox").on("touchend", function (event) {
+    if (isMove) {
+      isMove = false;
+      return;
+    }
+    $.post("http://192.168.31.130:5001/mouse_click", { button: "left" }, function (data, status) {
+      console.log("Data: " + data + "\nStatus: " + status);
+    });
+  });
+
+  $("#mediaButton").click(function () {
+    resetFooterSelect();
+    if ($("#mediaBox")[0].style.display == "grid") {
+      $("#mediaBox")[0].style.display = "none";
+      return;
+    }
+    $("#mediaButton").css("border", "1px solid #000000");
+    $("#mediaBox")[0].style.display = "grid";
+  });
+
+  $("#browserButton").click(function () {
+    resetFooterSelect();
+    if ($("#browserBox")[0].style.display == "grid") {
+      $("#browserBox")[0].style.display = "none";
+      return;
+    }
+    $("#browserButton").css("border", "1px solid #000000");
+    $("#browserBox")[0].style.display = "grid";
+  });
+
+  $("#keyboardButton").click(function () {
+    resetFooterSelect();
+    $("#keyboardBox").focus();
+    $("#test").text(initialViewportHeight + " - " + window.innerHeight);
+    setTimeout(() => {
+      $("#test").text(initialViewportHeight + " - " + window.innerHeight);
+    }, 1000);
+  });
+
+  $("#keyboardBox").on("keyup", function (e) {
+    if (e.key == "Enter") {
+      const val = $("#keyboardBox").val();
+      $.post("http://192.168.31.130:5001/keyboard_type", { keys: val }, function (data, status) {
+        console.log("Data: " + data + "\nStatus: " + status);
+      });
+      $("#keyboardBox")[0].value = "";
+    }
+  });
+
+  $("#functionButton").click(function () {
+    resetFooterSelect();
+    if ($("#functionBox")[0].style.display == "grid") {
+      $("#functionBox")[0].style.display = "none";
+      return;
+    }
+    $("#functionButton").css("border", "1px solid #000000");
+    $("#functionBox")[0].style.display = "grid";
+  });
+
+  $("#appListButton").click(function () {
+    resetFooterSelect();
+    $.post("http://192.168.31.130:5001/app_list", function (data, status) {
+      data.forEach((element) => {
+        $("#appListBox").append('<button class="appButton" onclick="openApp(\'' + element + "')\">" + element + "</button>");
+      });
+      if ($("#appListBox")[0].style.display == "grid") {
+        $("#appListBox")[0].style.display = "none";
+        return;
+      }
+      $("#appListButton").css("border", "1px solid #000000");
+      $("#appListBox")[0].style.display = "grid";
+    });
+  });
+});
+
+function keyPress(key) {
+  $.post("http://192.168.31.130:5001/press_key", { key: key }, function (data, status) {
+    console.log("Data: " + data + "\nStatus: " + status);
+  });
+}
+
+function mousePress(key) {
+  $.post("http://192.168.31.130:5001/mouse_click", { button: key }, function (data, status) {
+    console.log("Data: " + data + "\nStatus: " + status);
+  });
+}
+
+function openApp(appName) {
+  $.post("http://192.168.31.130:5001/app_select", { app_name: appName }, function (data, status) {
+    console.log("Data: " + data + "\nStatus: " + status);
+  });
+}
+
+function resetFooterSelect() {
+  $("#appListButton").css("border", "none");
+  $("#appListBox").css("display", "none");
+
+  $("#mediaButton").css("border", "none");
+  $("#mediaBox").css("display", "none");
+
+  $("#browserButton").css("border", "none");
+  $("#browserBox").css("display", "none");
+
+  $("#keyboardButton").css("border", "none");
+
+  $("#functionButton").css("border", "none");
+  $("#functionBox").css("display", "none");
+}
